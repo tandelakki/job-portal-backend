ed8f39fc1acc25e066b294baafeec0ef3874a7fb
akshay <your.correct.tandelakki111@gmail.com>
2025-04-29 15:15:37 +0530

    first commit

--- index.js
+++ index.js
@@ -20,9 +20,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+    origin: [
+      "http://localhost:5173",
+      "https://melodious-trifle-c56bff.netlify.app"
+    ],
+    credentials: true,
+  };
 app.use(cors(corsOptions));
 
 
